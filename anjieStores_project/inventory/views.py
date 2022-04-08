eca20189044f8988fa3870aa14dab75bdfc83f53
MikunsHub <63354714+MikunsHub@users.noreply.github.com>
2022-04-08 12:47:48 +0100

    filter feature added to products

--- anjieStores_project/inventory/views.py
+++ anjieStores_project/inventory/views.py
@@ -1,31 +1,23 @@
 from django.shortcuts import render
 from .models import Employee,Products,ProductType
-# def index(request):
-#     return HttpResponse("Hello, welcome to Anjie Stores")
-
+from .filters import ProductFilter
 
 
 def index(request):
-    # context = {
-    #     "order":kpi,
-    #     "Total_Sales":"190,000",
-    #     "Profit": "30,000"
-    # }
+    
     employee = Employee.objects.get(stateOfOrigin="Ekiti")
     productsType = ProductType.objects.all()
     products = Products.objects.all()
-    # for product in products:
-    #     print(product.productName)
-    #     print(product.Price)
-    print(productsType)
+
+    myFilter = ProductFilter(request.GET, queryset=products)
+    products = myFilter.qs
     context = {
         "staff_no":"1234",
         "full_name":"Ayomikun Ogunjuyigbe",
         "employee": employee,
         "productsType": productsType,
-        "products": products
+        "products": products,
+        "myFilter": myFilter
     }
     return render(request, 'inventory/index.html',context)
 
-# def user_mgt(request):
-#     pass
