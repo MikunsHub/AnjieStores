30c96a3daeecdd32f40275c608dc1046d50c2f7f
MikunsHub <63354714+MikunsHub@users.noreply.github.com>
2022-04-08 15:15:53 +0100

    filter added to categories field

--- anjieStores_project/inventory/filters.py
+++ anjieStores_project/inventory/filters.py
@@ -6,3 +6,11 @@ class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Products
         fields = '__all__'
+        exclude = ['Price','quantity','productsID','manufacturer']
+
+
+class ProductTypeFilter(django_filters.FilterSet):
+    class Meta:
+        model = ProductType
+        fields = '__all__'
+        exclude = ['productTypeID']
--- anjieStores_project/inventory/views.py
+++ anjieStores_project/inventory/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Employee,Products,ProductType
-from .filters import ProductFilter
+from .filters import ProductFilter,ProductTypeFilter
 
 
 def index(request):
@@ -10,14 +10,17 @@ def index(request):
     products = Products.objects.all()
 
     myFilter = ProductFilter(request.GET, queryset=products)
+    prodTypefilter = ProductTypeFilter(request.GET, queryset=productsType)
     products = myFilter.qs
+    productsType = prodTypefilter.qs
     context = {
         "staff_no":"1234",
         "full_name":"Ayomikun Ogunjuyigbe",
         "employee": employee,
         "productsType": productsType,
         "products": products,
-        "myFilter": myFilter
+        "myFilter": myFilter,
+        "prodTypefilter": prodTypefilter
     }
     return render(request, 'inventory/index.html',context)
 
